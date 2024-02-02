59322b76d3ca850295096940af8001ddc669e91c
PaoloTolentino <paolo.tolentino@ciit.edu.ph>
2024-02-02 21:20:04 +0800

    chore: cleanups

--- lib/home/views/widgets/home_app_bar.dart
+++ lib/home/views/widgets/home_app_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:quake_safe_app/theme/theme.dart';
 
 class HomeAppBar extends StatelessWidget {
@@ -13,39 +12,6 @@ class HomeAppBar extends StatelessWidget {
       toolbarHeight: 65.h,
       backgroundColor: QuakeSafeColors.primary,
       elevation: 0,
-      // flexibleSpace: Container(
-      //   decoration: BoxDecoration(
-      //     color: Colors.blue,
-      //     boxShadow: [
-      //       BoxShadow(
-      //         offset: const Offset(0, 4),
-      //         blurRadius: 4,
-      //         color: Colors.black.withOpacity(0.25),
-      //       ),
-      //     ],
-      //   ),
-      //   height: 31.h,
-      //   child: TextField(
-      //     style: GoogleFonts.urbanist(
-      //       fontSize: 10.sp,
-      //     ),
-      //     decoration: InputDecoration(
-      //       hintText: 'Search',
-      //       filled: true,
-      //       suffixIcon: const Icon(Icons.search),
-      //       fillColor: Colors.white,
-      //       contentPadding: EdgeInsets.symmetric(
-      //         horizontal: 12.sp,
-      //       ),
-      //       border: const OutlineInputBorder(
-      //         borderRadius: BorderRadius.all(
-      //           Radius.circular(8),
-      //         ),
-      //         borderSide: BorderSide.none,
-      //       ),
-      //     ),
-      //   ),
-      // ),
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
